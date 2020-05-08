3e06e3bdc50b5aa630a44cb6dba7bc4734fa3bcb
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-08 15:55:09 -0300

    Add solution for space-age

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -9,5 +9,23 @@ data Planet = Mercury
             | Uranus
             | Neptune
 
+-- | Number of seconds in a year.
+year :: Float
+year = 31557600.0
+
+-- | Return the orbital period for a planet.
+orbitalPeriod :: Planet -> Float
+orbitalPeriod Mercury = 0.2408467
+orbitalPeriod Venus = 0.61519726
+orbitalPeriod Earth = 1.0
+orbitalPeriod Mars = 1.8808158
+orbitalPeriod Jupiter = 11.862615
+orbitalPeriod Saturn = 29.447498
+orbitalPeriod Uranus = 84.016846
+orbitalPeriod Neptune = 164.79132
+
+earthYears :: Float -> Float
+earthYears seconds = seconds / year
+
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = error "You need to implement this function."
+ageOn planet seconds = earthYears seconds / orbitalPeriod planet
