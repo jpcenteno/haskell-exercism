432b022b54b97c0d97a0e78eada5f7d48cb88201
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-09 07:04:55 -0300

    Changed isogram to implementation based on sets

--- isogram/src/Isogram.hs
+++ isogram/src/Isogram.hs
@@ -1,13 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Isogram (isIsogram) where
 
-import Data.Char (toLower, isLetter)
+import           Data.Set (Set)
+import qualified Data.Set as Set
+import           Data.Char (toLower, isLetter)
 
 prepareString :: String -> String
-prepareString = filter isLetter . map toLower
+prepareString = map toLower . filter isLetter
 
-isIsogramAux :: String -> Bool
-isIsogramAux []     = True
-isIsogramAux (c:cs) = (not . elem c $ cs) && isIsogram cs
+isIsogramAux :: String -> Set Char -> Bool
+isIsogramAux ""     _   = True
+isIsogramAux (c:cs) set
+  | Set.member c set = False
+  | otherwise        = isIsogramAux cs $ Set.insert c set
 
 isIsogram :: String -> Bool
-isIsogram cs = isIsogramAux . prepareString $ cs
+isIsogram cs = isIsogramAux (prepareString cs) Set.empty
