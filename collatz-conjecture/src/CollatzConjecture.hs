46feb1fa5d48219fb734b566a0d002b2f0406174
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-08 21:28:05 -0300

    Add solution for collatz-conjecture

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,16 @@
 module CollatzConjecture (collatz) where
 
+nextCollatzNumber :: Integer -> Integer
+nextCollatzNumber n
+  | n `mod` 2 == 0 = n `div` 2
+  | otherwise      = 3 * n + 1
+
+collatzAux :: Integer -> Integer -> Integer
+collatzAux 0 _     = error ""
+collatzAux 1 steps = steps
+collatzAux n steps = collatzAux (nextCollatzNumber n) (steps + 1)
+
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz n
+  | n < 1     = Nothing
+  | otherwise = Just $ collatzAux n 0
