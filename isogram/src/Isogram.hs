e89e0c1303d44dcf7fababa17cf12d770b2e30ad
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-09 06:48:47 -0300

    Add solution for isogram

--- isogram/src/Isogram.hs
+++ isogram/src/Isogram.hs
@@ -1,4 +1,13 @@
 module Isogram (isIsogram) where
 
+import Data.Char (toLower, isLetter)
+
+prepareString :: String -> String
+prepareString = filter isLetter . map toLower
+
+isIsogramAux :: String -> Bool
+isIsogramAux []     = True
+isIsogramAux (c:cs) = (not . elem c $ cs) && isIsogram cs
+
 isIsogram :: String -> Bool
-isIsogram = error "You need to implement this function!"
+isIsogram cs = isIsogramAux . prepareString $ cs
