0f58182269d6c84b39d17a31bbc360edd7edce47
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-08 22:03:12 -0300

    Add solution for etl

--- etl/src/ETL.hs
+++ etl/src/ETL.hs
@@ -1,6 +1,12 @@
 module ETL (transform) where
 
-import Data.Map (Map)
+import Data.Map (Map, fromList, toList)
+import Data.Char (toLower)
+
+
+transformTuple :: a -> String -> [(Char, a)]
+transformTuple a = map (\c -> (toLower c, a))
 
 transform :: Map a String -> Map Char a
-transform legacyData = error "You need to implement this function."
+-- transform legacyData = undefined
+transform = fromList . concat . map (uncurry transformTuple) . toList
