2409b063425b05fc0ee2f6b0110075ae993edcb8
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-10 00:37:36 -0300

    Add solution for anagram.

--- anagram/src/Anagram.hs
+++ anagram/src/Anagram.hs
@@ -1,4 +1,21 @@
 module Anagram (anagramsFor) where
 
-anagramsFor :: String -> [String] -> [String]
-anagramsFor xs xss = error "You need to implement this function."
+import           Data.MultiSet (MultiSet)
+import qualified Data.MultiSet as MultiSet
+import           Data.Text (Text)
+import qualified Data.Text as T
+import GHC.Exts (toList)
+
+type CharCount = MultiSet Char
+
+charCount :: Text -> CharCount
+charCount = foldr MultiSet.insert MultiSet.empty . toList
+
+areAnagrams :: Text -> Text -> Bool
+areAnagrams s1 s2 =  s1' /= s2' && charCount s1' == charCount s2'
+  where
+    s1' = T.toLower s1
+    s2' = T.toLower s2
+
+anagramsFor :: Text -> [Text] -> [Text]
+anagramsFor str = filter $ areAnagrams str
