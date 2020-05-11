0078edd65401a44e4b275f0cdc71224d25a94002
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-11 20:53:45 -0300

    Add solution for rna-transcription

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -1,4 +1,20 @@
 module DNA (toRNA) where
 
+import Data.Either (partitionEithers)
+
+complementChar :: Char -> Either Char Char
+complementChar 'G' = Right 'C'
+complementChar 'C' = Right 'G'
+complementChar 'T' = Right 'A'
+complementChar 'A' = Right 'U'
+complementChar c   = Left c
+
+checkComplementedChars :: [Either Char Char] -> Either Char String
+checkComplementedChars es
+  | null lefts = Right rights
+  | otherwise  = Left . head $ lefts
+  where
+    (lefts, rights) = partitionEithers es
+
 toRNA :: String -> Either Char String
-toRNA xs = error "You need to implement this function."
+toRNA xs = checkComplementedChars . map complementChar $ xs
