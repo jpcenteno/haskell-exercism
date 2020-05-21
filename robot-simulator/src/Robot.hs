0315861a4444158ab72669c8544a4e71b206d2b8
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-21 01:28:56 -0300

    robot-simulator: make tests pass

--- robot-simulator/src/Robot.hs
+++ robot-simulator/src/Robot.hs
@@ -6,22 +6,56 @@ module Robot
     , move
     ) where
 
+-- Turnable ------------------------------------------------------------------
+
+-- | A type that can be turned left or right.
+class Turnable a where
+    turnRight :: a -> a
+    turnLeft  :: a -> a
+
+-- Bearing -------------------------------------------------------------------
+
 data Bearing = North
              | East
              | South
              | West
              deriving (Eq, Show)
 
-data Robot = Dummy
+instance Turnable Bearing where
+    turnRight North = East
+    turnRight East  = South
+    turnRight South = West
+    turnRight West  = North
+    turnLeft East  = North
+    turnLeft South = East
+    turnLeft West  = South
+    turnLeft North = West
 
-bearing :: Robot -> Bearing
-bearing robot = error "You need to implement this function."
+-- Robot ---------------------------------------------------------------------
 
-coordinates :: Robot -> (Integer, Integer)
-coordinates robot = error "You need to implement this function."
+data Robot = Robot { bearing     :: Bearing
+                   , coordinates :: (Integer, Integer)}
+                   deriving (Show)
+
+instance Turnable Robot where
+    turnRight robot = robot { bearing = turnRight $ bearing robot }
+    turnLeft  robot = robot { bearing = turnLeft  $ bearing robot }
+
+-- | Move the robot forward.
+advance :: Robot -> Robot
+advance Robot{bearing=North, coordinates=(x, y)} = Robot North (x    , y + 1)
+advance Robot{bearing=East,  coordinates=(x, y)} = Robot East  (x + 1, y    )
+advance Robot{bearing=South, coordinates=(x, y)} = Robot South (x    , y - 1)
+advance Robot{bearing=West,  coordinates=(x, y)} = Robot West  (x - 1, y    )
 
 mkRobot :: Bearing -> (Integer, Integer) -> Robot
-mkRobot direction coordinates = error "You need to implement this function."
+mkRobot = Robot -- Use the constructor
+
+performInstruction :: Char -> Robot -> Robot
+performInstruction 'R' = turnRight
+performInstruction 'L' = turnLeft
+performInstruction 'A' = advance
+performInstruction _   = error "unsupported instruction"
 
 move :: Robot -> String -> Robot
-move robot instructions = error "You need to implement this function."
+move robot instructions = foldl (flip performInstruction) robot instructions
