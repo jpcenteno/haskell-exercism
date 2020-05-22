3a5f56f9d3b1896c8f2b3c31b1c173a2b0dcd594
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-22 08:10:37 -0300

    leap: change solution to appease a mentor

--- leap/src/LeapYear.hs
+++ leap/src/LeapYear.hs
@@ -2,4 +2,4 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
 isLeapYear year =
-    (year `rem` 4 == 0) && (year `rem` 100 /= 0) || (year `rem` 400 == 0)
+    (year `rem` 4 == 0) && ((year `rem` 100 /= 0) || (year `rem` 400 == 0))
